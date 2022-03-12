cd07bac388c3b6d219d063af514b5999e2ac65cb
Dima Zap <zapilenovd@gmail.com>
2022-03-12 10:46:09 +0300

    Planing day add

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -2,6 +2,7 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import javax.lang.model.element.NestingKind;
 import java.time.Duration;
@@ -11,28 +12,24 @@ import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
-    public LocalDate today = LocalDate.now();
-    public String setDataDefault = today.plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    String generateDate (int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+    String planingDate= generateDate(4);
 
     @Test
     public void shouldCardReg() {
         Configuration.holdBrowserOpen=true;
-        Configuration.timeout=50;
         open("http://localhost:9999/");
         $("[type=\"text\"]").setValue("Ярославль");
-        $("[data-test-id='date'] input").doubleClick().setValue(String.valueOf(setDataDefault));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planingDate);
         $x("//*[@name=\"name\"]").setValue("Бахмет Олег");
         $("[data-test-id=\"phone\"] input").setValue("+79066869535");
         $("span.checkbox__box").click();
         $x("//*[text()=\"Забронировать\"]").click();
         $("[class=\"notification__title\"]").shouldBe(Condition.visible, Duration.ofSeconds(12));
-        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + setDataDefault));
-
-
-
-
-
-
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planingDate));
 
     }
 }
